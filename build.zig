459596450fe4817db8ef38a6096493349935c905
Yu Li <liyu1981@gmail.com>
2024-02-06 23:56:35 +1100

    compress share.tgz.bin in build.zig

--- build.zig
+++ build.zig
@@ -58,6 +58,9 @@ pub fn build(b: *std.Build) !void {
     var bison_bin_step = b.step("bison_bin", "copy bison/bison/src/bison as src/bison.bin");
     bison_bin_step.makeFn = bisonBinStepMakeFn;
 
+    var bison_share_bin_step = b.step("bison_share_bin", "compress share and save to share.tgz.bin");
+    bison_share_bin_step.makeFn = bisonShareBinStepMakeFn;
+
     zison_exe.step.dependOn(bison_bin_step);
     zison_exe.step.dependOn(&m4phony.step);
     zison_exe.step.dependOn(&libbison_a.step);
@@ -68,7 +71,7 @@ pub fn build(b: *std.Build) !void {
     b.installArtifact(zison_exe);
 }
 
-fn flexBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) anyerror!void {
+fn flexBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) !void {
     _ = node;
     _ = step;
     var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -76,29 +79,37 @@ fn flexBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) anyerror!v
     const allocator = aa.allocator();
 
     {
-        const result = try zcmd.run(.{ .allocator = allocator, .commands = &[_][]const []const u8{
-            &[_][]const u8{
-                "cp",
-                g_build.pathFromRoot("flex/flex/src/flex"),
-                g_build.pathFromRoot("src/flex.bin"),
+        const result = try zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &[_][]const u8{
+                    "cp",
+                    g_build.pathFromRoot("flex/flex/src/flex"),
+                    g_build.pathFromRoot("src/flex.bin"),
+                },
             },
-        } });
+            .cwd = g_build.pathFromRoot(""),
+        });
         result.assertSucceededPanic(.{});
     }
 
     {
-        const result = try zcmd.run(.{ .allocator = allocator, .commands = &[_][]const []const u8{
-            &[_][]const u8{
-                "chmod",
-                "0644",
-                g_build.pathFromRoot("src/flex.bin"),
+        const result = try zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &[_][]const u8{
+                    "chmod",
+                    "0644",
+                    g_build.pathFromRoot("src/flex.bin"),
+                },
             },
-        } });
+            .cwd = g_build.pathFromRoot(""),
+        });
         result.assertSucceededPanic(.{});
     }
 }
 
-fn bisonBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) anyerror!void {
+fn bisonBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) !void {
     _ = node;
     _ = step;
     var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -106,24 +117,70 @@ fn bisonBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) anyerror!
     const allocator = aa.allocator();
 
     {
-        const result = try zcmd.run(.{ .allocator = allocator, .commands = &[_][]const []const u8{
-            &[_][]const u8{
-                "cp",
-                g_build.pathFromRoot("bison/bison/src/bison"),
-                g_build.pathFromRoot("src/bison.bin"),
+        const result = try zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &[_][]const u8{
+                    "cp",
+                    g_build.pathFromRoot("bison/bison/src/bison"),
+                    g_build.pathFromRoot("src/bison.bin"),
+                },
             },
-        } });
+            .cwd = g_build.pathFromRoot(""),
+        });
         result.assertSucceededPanic(.{});
     }
 
     {
-        const result = try zcmd.run(.{ .allocator = allocator, .commands = &[_][]const []const u8{
-            &[_][]const u8{
-                "chmod",
-                "0644",
-                g_build.pathFromRoot("src/bison.bin"),
+        const result = try zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &[_][]const u8{
+                    "chmod",
+                    "0644",
+                    g_build.pathFromRoot("src/bison.bin"),
+                },
             },
-        } });
+            .cwd = g_build.pathFromRoot(""),
+        });
+        result.assertSucceededPanic(.{});
+    }
+}
+
+fn bisonShareBinStepMakeFn(step: *std.Build.Step, node: *std.Progress.Node) !void {
+    _ = node;
+    _ = step;
+    var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer aa.deinit();
+    const allocator = aa.allocator();
+
+    {
+        const result = try zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &[_][]const u8{
+                    "tar",
+                    "czf",
+                    "src/share.tgz.bin",
+                },
+            },
+            .cwd = g_build.pathFromRoot(""),
+        });
+        result.assertSucceededPanic(.{});
+    }
+
+    {
+        const result = try zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &[_][]const u8{
+                    "chmod",
+                    "0644",
+                    g_build.pathFromRoot("src/share.tgz.bin"),
+                },
+            },
+            .cwd = g_build.pathFromRoot(""),
+        });
         result.assertSucceededPanic(.{});
     }
 }
