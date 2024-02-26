69589450e6039c5404449f4f533e5b2600cf1f12
Yu Li <liyu1981@gmail.com>
2024-02-26 15:32:12 +1100

    rename tests, and delete some old unnecessary files

--- tests/flex_reccalc/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f zlexison.zig
-../../zig-out/bin/zison zlexison -o zlexison.zig ../bison_reccalc/parser.y
-../../zig-out/bin/zlex -o scan.zig -z zlexison.zig scan.l
-zig build-exe scan.zig
